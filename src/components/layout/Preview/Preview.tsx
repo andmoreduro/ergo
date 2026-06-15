153f86d94f6652268ce59d51e7e868ad1acce8d0
Ergo Dev <ergo-dev@local>
2026-06-14 21:08:58 -0500

    perf(preview): experiment with only first 3 pages in DOM

--- src/components/layout/Preview/Preview.tsx
+++ src/components/layout/Preview/Preview.tsx
@@ -636,7 +636,7 @@ export const Preview = ({
                     >
                     <div className={styles.svgContainer}>
                         {previewPages.length > 0 && previewRevision !== null ? (
-                            previewPages.map((page, index) => {
+                            previewPages.slice(0, 3).map((page, index) => {
                                 const pageNumber = page.page_number;
                                 return (
                                     <PreviewPageSvg
